eaff6f215e302642ab67e3fe6908527ab3c86ae5
YilmazBatal <b.ogulbey@gmail.com>
2026-07-20 11:07:44 +0300

    Added Easteregg

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -89,7 +89,6 @@ public class AudioManager : MonoBehaviour
         {
             musicSource.clip = s.clip;
             musicSource.Play();
-            Debug.LogWarning($"Supposed to play: {musicName}");
         }
         else
         {
--- Assets/Scripts/Managers/MySceneManager.cs
+++ Assets/Scripts/Managers/MySceneManager.cs
@@ -45,7 +45,6 @@ public class MySceneManager : MonoBehaviour
     
     private void MusicPlayer()
     {
-        Debug.Log("Music Player func called.");
         if (SceneManager.GetActiveScene().buildIndex == 0)
         {
             AudioManager.Instance.PlayMusic("MenuMusic");
@@ -54,5 +53,8 @@ public class MySceneManager : MonoBehaviour
             AudioManager.Instance.PlayMusic("GameMusic");
         }
     }
-    
+    public void PlayEasterEgg()
+    {
+        AudioManager.Instance.PlaySFX("fart");
+    }
 }
